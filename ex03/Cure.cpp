904666ab6404beb2b445e18390c08c3e0a91235a
Escudo5 <sergiomargar03@gmail.com>
2025-09-18 17:54:50 +0200

    a

--- ex03/Cure.cpp
+++ ex03/Cure.cpp
@@ -6,7 +6,7 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/18 17:20:25 by smarquez          #+#    #+#             */
-/*   Updated: 2025/09/18 17:51:56 by smarquez         ###   ########.fr       */
+/*   Updated: 2025/09/18 17:54:46 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,5 +37,6 @@ Cure::~Cure()
 
 AMateria *Cure::clone() const
 {
-    
+    return new Cure (*this);
+
 }
